37b9177b72e8cdd04da640d21eb7acfc744e59d4
Paul <paul.rehmet@tum.de>
2025-01-29 11:44:21 +0100

    Use doorbell address from io_cq

--- src/nvme_future.rs
+++ src/nvme_future.rs
@@ -42,16 +42,8 @@ impl Future for NvmeFuture<'_> {
             Some(val) => {
                 if this.c_id == val.c_id {
                     let (head, prev) = this.io_cq.new_head();
-
                     unsafe {
-                        //std::ptr::write_volatile(this.io_cq.doorbell as *mut u32, head as u32);
-                        std::ptr::write_volatile(
-                            (this.addr as usize
-                                + 0x1000
-                                + (4 << this.dstrd) * (2 * this.q_id) as usize)
-                                as *mut u32,
-                            head as u32,
-                        );
+                        std::ptr::write_volatile(this.io_cq.doorbell as *mut u32, head as u32);
                     }
                     return Poll::Ready((head, val, prev));
                 }
