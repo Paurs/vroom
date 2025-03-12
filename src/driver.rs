1e89c190a1b89c3584576099229872e216900d59
Paul <paul.rehmet@tum.de>
2025-03-12 11:31:19 +0100

    Remove pairs from Driver

--- src/driver.rs
+++ src/driver.rs
@@ -1,14 +1,13 @@
 use std::error::Error;
 
-use crate::{pci::*, NvmeDevice, NvmeQueuePair, QUEUE_LENGTH};
+use crate::{pci::*, NvmeDevice};
 
 pub struct Driver {
     pub nvme: NvmeDevice,
-    pub pairs: Vec<NvmeQueuePair>,
 }
 
 impl Driver {
-    pub fn new(pci_addr: &str, queue_num: usize) -> Result<Self, Box<dyn Error>> {
+    pub fn new(pci_addr: &str) -> Result<Self, Box<dyn Error>> {
         let mut vendor_file = pci_open_resource_ro(pci_addr, "vendor").expect("wrong pci address");
         let mut device_file = pci_open_resource_ro(pci_addr, "device").expect("wrong pci address");
         let mut config_file = pci_open_resource_ro(pci_addr, "config").expect("wrong pci address");
@@ -31,39 +30,6 @@ impl Driver {
             nvme.identify_namespace(n);
         }
 
-        let mut pairs = Vec::new();
-        for _ in 0..queue_num {
-            let qp = nvme.create_io_queue_pair(QUEUE_LENGTH)?;
-            pairs.push(qp);
-        }
-
-        Ok(Driver {
-            nvme: nvme,
-            pairs: pairs,
-        })
-    }
-
-    pub async fn listen(&mut self) {
-        loop {
-            self.poll_queue(1).await;
-        }
-    }
-
-    pub async fn poll_queue(&mut self, q_id: u16) {
-        if let Some(pair) = self
-            .pairs
-            .iter_mut()
-            .find(|&&mut NvmeQueuePair { id, .. }| id == q_id)
-        {
-            pair.poll().await;
-        } else {
-            println!("not found");
-        }
-    }
-
-    pub async fn poll(&mut self) {
-        for p in self.pairs.iter_mut() {
-            p.poll().await;
-        }
+        Ok(Driver { nvme: nvme })
     }
 }
